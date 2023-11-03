04bb4573eda725178b5dafffe9b9387c293d8e72
saifularafat <saifularafat.info@gmail.com>
2023-11-03 22:27:04 +0600

    error page content is added

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -5,10 +5,11 @@ const Error = () => {
     const { isDarkMode } = Theme();
     return (
         <div className={`${isDarkMode ? 'bg-slate-800' : 'bg-blue-100 text-slate-800'}  h-screen overflow-hidden flex items-center justify-center text-accent`}>
-            <div className=" ">
-                <h2>404 Error</h2>
-                <Link to="/">
-                    <button>Go Back</button>
+            <div className="text-center">
+                <h2 className="md:text-lg text-base font-semibold">Page Not Found 🦇</h2>
+                <p className="md:text-base text-sm font-normal py-3">Oops! 😓 The requested URL was not found on this server.</p>
+                <Link to="/" className="">
+                    <button className="text-sm md:text-base font-medium py-3 px-6 tracking-widest bg-primary rounded-lg shadow-2xl text-white hover:bg-blue-500 transition-all duration-300 mt-3">Go Back</button>
                 </Link>
             </div>
 
