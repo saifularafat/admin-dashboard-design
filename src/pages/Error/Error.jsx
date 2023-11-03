2143c83793973f39855df25fffc9d9dc35a1b153
saifularafat <saifularafat.info@gmail.com>
2023-11-03 23:11:10 +0600

    error animation is add

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,18 +1,21 @@
 import { Link } from "react-router-dom";
 import Theme from "../Shared/Theme";
-
+import error from "../../assets/animation/error.json"
+import Lottie from "lottie-react";
 const Error = () => {
     const { isDarkMode } = Theme();
     return (
-        <div className={`${isDarkMode ? 'bg-slate-800' : 'bg-blue-100 text-slate-800'}  h-screen overflow-hidden flex items-center justify-center text-accent`}>
-            <div className="text-center">
-                <h2 className="md:text-lg text-base font-semibold">Page Not Found 🦇</h2>
-                <p className="md:text-base text-sm font-normal py-3">Oops! 😓 The requested URL was not found on this server.</p>
-                <Link to="/" className="">
-                    <button className="text-sm md:text-base font-medium py-3 px-6 tracking-widest bg-primary rounded-lg shadow-2xl text-white hover:bg-blue-500 transition-all duration-300 mt-3">Go Back</button>
-                </Link>
+        <div className={`${isDarkMode ? 'bg-slate-800' : 'bg-blue-100 text-slate-800'}  h-screen overflow-hidden flex items-center justify-center text-accent pt-9`}>
+            <div>
+                <div className="text-center">
+                    <h2 className="md:text-lg text-base font-semibold">Page Not Found 🦇</h2>
+                    <p className="md:text-base text-sm font-normal py-3">Oops! 😓 The requested URL was not found on this server.</p>
+                    <Link to="/" className="">
+                        <button className="text-sm md:text-base font-medium py-3 px-6 tracking-widest bg-primary rounded-lg shadow-2xl text-white hover:bg-blue-500 transition-all duration-300 mt-3">Go Back</button>
+                    </Link>
+                </div>
+                <Lottie animationData={error} loop={true} className="h-2/4"/>;
             </div>
-
         </div>
     );
 };
