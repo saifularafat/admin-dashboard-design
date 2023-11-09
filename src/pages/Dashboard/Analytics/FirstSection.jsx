98c723b5ede8e68e004532c29d77dfd5b6240099
saifularafat <saifularafat.info@gmail.com>
2023-11-10 01:04:28 +0600

    sales overview is completet

--- src/pages/Dashboard/Analytics/FirstSection.jsx
+++ src/pages/Dashboard/Analytics/FirstSection.jsx
@@ -1,3 +1,4 @@
+import { AiOutlineLink, AiOutlineShoppingCart } from "react-icons/ai";
 import Theme from "../../Shared/Theme";
 import SwiperAnaly from "./SwiperAnaly";
 
@@ -9,7 +10,34 @@ const FirstSection = () => {
                 <SwiperAnaly />
             </div>
             <div className={`${isDarkMode ? 'bg-slate-900' : 'bg-white'} w-full text-accent shadow-2xl p-4 overflow-hidden rounded-xl`}>
-
+                <div className="flex items-center justify-between">
+                    <h2 className="text-sm">Sales Overview</h2>
+                    <p className="text-sm font-semibold">+18.2%</p>
+                </div>
+                <h2 className="text-2xl font-semibold pt-2">$42.5k</h2>
+                <div className="flex items-center justify-between   mt-3">
+                    <div className="border-r-2 border-0 border-solid border-slate-300">
+                        <div className="flex items-center gap-2 ">
+                            <div className="bg-blue-300 rounded-lg p-1 mt-2 ">
+                                <AiOutlineShoppingCart className="text-blue-700 text-xl" />
+                            </div>
+                            <p className=" font-medium text-slate-500 mr-2">Order</p>
+                        </div>
+                        <h4 className="text-xl font-semibold pt-1">62.2%</h4>
+                        <p className="text-sm text-slate-400">6,440</p>
+                    </div>
+                    <div className="">
+                        <div className="flex items-center gap-2 ">
+                            <p className=" font-medium text-slate-500 mr-2">Visits</p>
+                            <div className="bg-blue-300 rounded-lg p-1 mt-2 ">
+                                <AiOutlineLink className="text-blue-700 text-xl" />
+                            </div>
+                        </div>
+                        <h4 className="text-xl text-right font-semibold pt-1">25.5%</h4>
+                        <p className="text-sm text-right text-slate-400">12,749</p>
+                    </div>
+                </div>
+                <progress className="progress progress-info w-56 mt-3" value="70" max="100"></progress>
             </div>
             <div className={`${isDarkMode ? 'bg-slate-900' : 'bg-white'} w-full text-accent shadow-2xl p-4 overflow-hidden rounded-xl`}>
 
--- src/pages/Dashboard/Analytics/SwiperAnaly.jsx
+++ src/pages/Dashboard/Analytics/SwiperAnaly.jsx
@@ -30,6 +30,44 @@ const SwiperAnaly = () => {
                     <div className='grid grid-cols-3'>
                         <div className='col-span-2 text-white py-2'>
                             <h3 className='text-left text-lg font-semibold leading-none'>Website Analytics <br /> <span className='text-sm font-medium'>Total 28.5% Conversion Rate</span></h3>
+                            <h4 className='pt-8 text-lg font-semibold'>Traffic</h4>
+                            <p className='pt-4'>
+                                <span className='bg-black/20 py-1 px-2 rounded-lg font-medium mr-1'>1.7k</span><span className='pl-1 font-medium mr-5'>Sessions</span>
+                                <span className='bg-black/20 py-1 px-2 rounded-lg font-medium mr-1'>3.5k</span><span className='pl-1 font-medium'>Page View</span>
+                            </p>
+                            <p className='pt-5'>
+                                <span className='bg-black/20 py-1 px-4 rounded-lg font-medium mr-1'>1.2k</span><span className='pl-2 font-medium mr-5'>Leads</span>
+                                <span className='bg-black/20 py-1 px-4 rounded-lg font-medium mr-1'>12%</span><span className='pl-2 font-medium mr-5'>Conversions</span>
+                            </p>
+                        </div>
+                        <div className='col-span-1  w-36 h-36'>
+                            <img src={slider1} alt="" className='object-cover' />
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='grid grid-cols-3'>
+                        <div className='col-span-2 text-white py-2'>
+                            <h3 className='text-left text-lg font-semibold leading-none'>Website Analytics <br /> <span className='text-sm font-medium'>Total 28.5% Conversion Rate</span></h3>
+                            <h4 className='pt-8 text-lg font-semibold'>Spending</h4>
+                            <p className='pt-4'>
+                                <span className='bg-black/20 py-1 px-4 rounded-lg font-medium mr-1'>12h</span><span className='pl-2 font-medium mr-5'>Spend</span>
+                                <span className='bg-black/20 py-1 px-4 rounded-lg font-medium mr-1'>182</span><span className='pl-2 font-medium'>Order Size</span>
+                            </p>
+                            <p className='pt-5'>
+                                <span className='bg-black/20 py-1 px-4 rounded-lg font-medium mr-1'>127</span><span className='pl-2 font-medium mr-5'>Order</span>
+                                <span className='bg-black/20 py-1 px-4 rounded-lg font-medium mr-1'>23k</span><span className='pl-2 font-medium'>Items</span>
+                            </p>
+                        </div>
+                        <div className='col-span-1  w-36 h-36'>
+                            <img src={slider2} alt="" className='object-cover' />
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='grid grid-cols-3'>
+                        <div className='col-span-2 text-white py-2'>
+                            <h3 className='text-left text-lg font-semibold leading-none'>Website Analytics <br /> <span className='text-sm font-medium'>Total 28.5% Conversion Rate</span></h3>
                             <h4 className='pt-8 text-lg font-semibold'>Revenue Sources</h4>
                             <p className='pt-4'>
                                 <span className='bg-black/20 py-1 px-4 rounded-lg font-medium mr-1'>268</span><span className='pl-2 font-medium mr-5'>Direct</span>
@@ -40,13 +78,11 @@ const SwiperAnaly = () => {
                                 <span className='bg-black/20 py-1 px-4 rounded-lg font-medium mr-1'>1.2</span><span className='pl-2 font-medium mr-5'>kCampaign</span>
                             </p>
                         </div>
-                        <div className='col-span-1  w-32 h-32'>
-                            <img src={slider1} alt="" className='object-cover' />
+                        <div className='col-span-1  w-36 h-36'>
+                            <img src={slider3} alt="" className='object-cover' />
                         </div>
                     </div>
                 </SwiperSlide>
-                {/* <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide> */}
             </Swiper>
         </>
     );
