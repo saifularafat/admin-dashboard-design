0f65ee912a6bedab4c8872d1918249cccc0407ff
saifularafat <saifularafat.info@gmail.com>
2023-11-13 22:37:08 +0600

    email search bar is completet

--- src/pages/Dashboard/Emails/EmailRightSite/EmailRightSite.jsx
+++ src/pages/Dashboard/Emails/EmailRightSite/EmailRightSite.jsx
@@ -1,10 +1,14 @@
+import { BsSearch } from "react-icons/bs";
 import useTheme from "../../../Shared/useTheme";
 
 const EmailRightSite = () => {
     const { isDarkMode } = useTheme();
     return (
         <div className={`${isDarkMode ? 'bg-slate-900' : 'bg-white'} text-accent rounded-r-xl shadow-2xl p-2`}>
-            <h3>This a right site Email</h3>
+            <div className="flex items-center py-2">
+                <BsSearch className="text-xl font-bold mx-3"/>
+                <input className="" type="text" placeholder="Search Email " />
+            </div>
         </div>
     );
 };
--- src/pages/Dashboard/Emails/Emails.jsx
+++ src/pages/Dashboard/Emails/Emails.jsx
@@ -4,11 +4,11 @@ import EmailRightSite from "./EmailRightSite/EmailRightSite";
 const Emails = () => {
     
     return (
-        <div className="grid grid-cols-3 md:first-letter:grid-cols-5 gap-[2px] mt-2 py-2">
-            <div className={`col-span-1 md:grid-cols-2`}>
+        <div className="grid grid-cols-3 md:grid-cols-8 gap-[2px] mt-2 w-full py-2">
+            <div className={`col-span-1 md:col-span-2`}>
                 <EmailLeftSite />
             </div>
-            <div className={`col-span-2 md:grid-cols-3`}>
+            <div className={`col-span-2 md:col-span-6`}>
                 <EmailRightSite />
             </div>
         </div>
