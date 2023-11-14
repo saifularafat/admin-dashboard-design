42dfbb41129e35b06dcdb78c5aa82263c08fba09
saifularafat <saifularafat.info@gmail.com>
2023-11-15 01:51:09 +0600

    email compose and footer add

--- src/pages/Dashboard/Emails/Emails.jsx
+++ src/pages/Dashboard/Emails/Emails.jsx
@@ -1,16 +1,20 @@
+import Footer from "../../Shared/Footer";
 import EmailLeftSite from "./EmailLeftSite/EmailLeftSite";
 import EmailRightSite from "./EmailRightSite/EmailRightSite";
 
 const Emails = () => {
-    
+
     return (
-        <div className="grid grid-cols-3 md:grid-cols-8 gap-1 mt-2 w-full py-2">
-            <div className={`col-span-1 md:col-span-2`}>
-                <EmailLeftSite />
+        <div>
+            <div className="grid grid-cols-3 md:grid-cols-8 gap-1 mt-2 w-full py-2">
+                <div className={`col-span-1 md:col-span-2`}>
+                    <EmailLeftSite />
+                </div>
+                <div className={`col-span-2 md:col-span-6`}>
+                    <EmailRightSite />
+                </div>
             </div>
-            <div className={`col-span-2 md:col-span-6`}>
-                <EmailRightSite />
-            </div>
+            <Footer />
         </div>
     );
 };
