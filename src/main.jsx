885faa498e880732f697f124eb4cb199a171997e
saifularafat <saifularafat.info@gmail.com>
2025-02-17 20:43:26 +0600

    router setup error fiexd

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import router from './router/router'
+import router from './router/Router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
