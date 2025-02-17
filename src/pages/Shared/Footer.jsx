54dc3fd879cfb4630a55f09a678b95ddababb4a0
saifularafat <saifularafat.info@gmail.com>
2025-02-17 19:58:25 +0600

    Updated footer and other code changes

--- src/pages/Shared/Footer.jsx
+++ src/pages/Shared/Footer.jsx
@@ -1,11 +1,11 @@
-import {GiSelfLove }from 'react-icons/gi'
+import { GiSelfLove } from 'react-icons/gi'
 const Footer = () => {
     return (
         <div className="pt-3 pb-0 md:flex items-center justify-between">
-            <p className='text-xs md:text-sm text-accent '>COPYRIGHT ©2023 Saiful Islam, All rights Reserved</p>
+            <p className='text-xs md:text-sm text-accent '>COPYRIGHT ©2024 Saiful Islam, All rights Reserved</p>
             <div className="flex items-center md:gap-4 gap-1">
                 <p className='text-xs text-accent'>Hand-crafted & Made with</p>
-                <GiSelfLove className='text-2xl text-red-600'/>
+                <GiSelfLove className='text-2xl text-red-600' />
             </div>
         </div>
     );
